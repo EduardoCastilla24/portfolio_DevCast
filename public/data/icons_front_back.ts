b65c59b91b37cdbcc6e5f62fe1c7257c1df0b6a7
Eduardo <edcast2430@gmail.com>
2025-09-15 00:19:09 -0500

    Add new SVG icons and implement logo components for front-end and tools
    - Added SVG icons for React, Supabase, Tailwind, TypeScript, and Vercel.
    - Created  and  to manage icon data.
    - Developed  component to display logos with hover effects.
    - Implemented  and  sections in the main layout.
    - Updated , , , and  components for improved styling and structure.
    - Introduced  component for project tags.
    - Enhanced global styles with animations and scrollbar customization.

---
+++ public/data/icons_front_back.ts
@@ -0,0 +1,53 @@
+interface Logo {
+  label: string;
+  icon: string;
+}
+
+const logos_front_back: Logo[] = [
+  {
+    label: "Astro",
+    icon: "/assets/icons/astro.svg",
+  },
+  {
+    label: "Html",
+    icon: "/assets/icons/html.svg",
+  },
+  {
+    label: "Css",
+    icon: "/assets/icons/css.svg",
+  },
+  {
+    label: "JavaScript",
+    icon: "/assets/icons/js.svg",
+  },
+  {
+    label: "Tailwind CSS",
+    icon: "/assets/icons/tailwind.svg",
+  },
+  {
+    label: "TypeScript",
+    icon: "/assets/icons/typescript.svg",
+  },
+  {
+    label: "React",
+    icon: "/assets/icons/react.svg",
+  },
+  {
+    label: "Laravel",
+    icon: "/assets/icons/laravel.svg",
+  },
+  {
+    label: "Php",
+    icon: "/assets/icons/php.svg",
+  },
+  {
+    label: "MySql",
+    icon: "/assets/icons/mysql.svg",
+  },
+  {
+    label: "PostgreSQL",
+    icon: "/assets/icons/postgre.svg",
+  },
+];
+
+export default logos_front_back;
---
+++ public/data/icons_tools.ts
@@ -0,0 +1,45 @@
+interface Logo {
+  label: string;
+  icon: string;
+}
+
+const logos_tools: Logo[] = [
+  {
+    label: "Git",
+    icon: "/assets/icons/git.svg",
+  },
+  {
+    label: "Postman",
+    icon: "/assets/icons/postman.svg",
+  },
+  {
+    label: "Figma",
+    icon: "/assets/icons/figma.svg",
+  },
+  {
+    label: "Supabase",
+    icon: "/assets/icons/supabase.svg",
+  },
+  {
+    label: "Vercel",
+    icon: "/assets/icons/vercel.svg",
+  },
+  {
+    label: "Netlify",
+    icon: "/assets/icons/netlify.svg",
+  },
+  {
+    label: "Illustrator",
+    icon: "/assets/icons/illustrator.svg",
+  },
+  {
+    label: "Photoshop",
+    icon: "/assets/icons/photoshop.svg",
+  },
+  {
+    label: "Corel Draw",
+    icon: "/assets/icons/coreldraw.svg",
+  },
+];
+
+export default logos_tools;
